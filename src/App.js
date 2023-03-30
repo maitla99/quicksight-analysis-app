96a64c22772486fbfdcb163e4ecba2ec6f07defd
maitla99 <maitla99@gmail.com>
2023-03-30 13:48:52 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [dataList, setDataList] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    fetch("http://localhost:3000/data")
+      .then((response) => response.json())
+      .then((data) => {
+        setDataList(data.AnalysisSummaryList);
+      });
+  };
+
+  const handleRefresh = () => {
+    fetchData();
+  };
+
+  const handleBackup = () => {
+    fetch("http://localhost:3000/backup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ dataList }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log("Backup success:", data));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My App</h1>
+      <button onClick={handleRefresh}>Refresh</button>
+      <ul>
+        {dataList &&
+          dataList.map((data, index) => <li key={index}>{data.Name}</li>)}
+      </ul>
+      <button onClick={handleBackup}>Backup</button>
     </div>
   );
 }
